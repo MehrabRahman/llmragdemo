cb9d28bdd0c2724eba7b4dc5825a066fbccb3132
MehrabRahman <mehrab.rahman@revature.com>
2024-01-17 12:58:38 -0800

    Creating instructions

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project 1
+Fork this project and refactor. Use a HuggingFace model of your choosing, set your environment variable for 'API_TOKEN' (which you get from your Profile -> Access Tokens menu on HuggingFace). 
+
+Ways to refactor:
+- Expand the application to multiple files (libraries)
+- Expand to a main function + various other methods
+- Expand to OOP
+- Load documents (txt files, etc) as context, to simulate a RAG
+- Expand to incorporate a Text UI
--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from transformers import pipeline
+import os
+import requests
 
-pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
+API_TOKEN = os.environ["API_TOKEN"] #Set a API_TOKEN environment variable before running
+API_URL = "" #Add a URL for a model of your choosing
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-messages = [
-    {
-        "role": "user",
-        "content": "How many chairs can a human eat in one sitting?"
+def query(prompt):
+    payload = {
+        "inputs": prompt,
+        "parameters": { #Try and experiment with the parameters
+            "max_new_tokens": 1024,
+            "temperature": 0.6,
+            "top_p": 0.9,
+            "do_sample": False,
+            "return_full_text": False
+        }
     }
-]
-prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-print(outputs[0]["generated_text"])
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()[0]['generated_text']
+
+question = "What is the population of Jacksonville, Florida?"
+context = "As of the most current census, Jacksonville, Florida has a population of 1 million."
+prompt = f"""Use the following context to answer the question at the end.
+
+{context}
+
+Question: {question}
+"""
+
+print(query(prompt))
