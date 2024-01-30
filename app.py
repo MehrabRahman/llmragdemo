96c43e24d8bccba18e924374174e7ad0fdfbccfb
MehrabRahman <mehrab.rahman@revature.com>
2024-01-30 11:04:40 -0800

    Updating with custom embedder

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 import requests
 import chromadb
+from embedder import CustomEmbedder
 
 def query(prompt):
     API_TOKEN = os.environ["API_TOKEN"] #Set a API_TOKEN environment variable before running
-    API_URL = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta" #Add a URL for a model of your choosing
+    API_URL = "https://z8dvl7fzhxxcybd8.eu-west-1.aws.endpoints.huggingface.cloud/" #Add a URL for a model of your choosing
     headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
     payload = {
@@ -20,18 +21,21 @@ def query(prompt):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()[0]['generated_text']
 
+def as_retriever(collection, query):
+    results = collection.query(
+        query_texts=[query],
+        n_results=3
+    )
+    return results['documents']
+
 def main():
+    custom_embedder = CustomEmbedder()
     client = chromadb.PersistentClient(path="../testdb")
-    collection = client.get_collection(name="test_collection")
+    collection = client.get_collection(name="test_collection", embedding_function=custom_embedder)
 
     question = "Do I have a pet?"
 
-    results = collection.query(
-        query_texts=[question],
-        n_results=3
-    )
-
-    context = results['documents']
+    context = as_retriever(collection, question)
     prompt = f"""Use the following context to answer the question at the end. Stop when you've answered the question. Do not generate any more than that.
 
     {context}
--- embedder.py
+++ embedder.py
@@ -1,4 +1,22 @@
+import os
 import chromadb
+import requests
+
+from chromadb.api.types import (
+    Documents,
+    EmbeddingFunction,
+    Embeddings
+)
+
+class CustomEmbedder(EmbeddingFunction[Documents]):
+    def __call__(self, input: Documents) -> Embeddings:
+        rest_client = requests.Session()
+        API_TOKEN = os.environ["API_TOKEN"] #Set a API_TOKEN environment variable before running
+        API_URL = "https://c9ejquzh6yum3xqf.us-east-1.aws.endpoints.huggingface.cloud/"
+        response = rest_client.post(
+            API_URL, json={"inputs": input}, headers={"Authorization": f"Bearer {API_TOKEN}"}
+        ).json()
+        return response
 
 def main():
     dataset = [
@@ -7,8 +25,10 @@ def main():
         "I need to download more RAM."
     ]
     chroma_client = chromadb.PersistentClient(path="../testdb")
+
+    custom_embedder = CustomEmbedder()
     chroma_client.delete_collection("test_collection")
-    collection = chroma_client.create_collection(name="test_collection")
+    collection = chroma_client.create_collection(name="test_collection", embedding_function=custom_embedder)
     
     for i, dataset_id in enumerate(dataset):
         collection.add(
