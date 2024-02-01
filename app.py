75f4a11c4b3bd3b83abf1e61ebcaf843b6422ed7
MehrabRahman <mehrab.rahman@revature.com>
2024-02-01 11:05:46 -0800

    Refactoring to more objects

--- app.py
+++ app.py
@@ -3,39 +3,43 @@ import requests
 import chromadb
 from embedder import CustomEmbedder
 
-def query(prompt):
-    API_TOKEN = os.environ["API_TOKEN"] #Set a API_TOKEN environment variable before running
-    API_URL = "https://z8dvl7fzhxxcybd8.eu-west-1.aws.endpoints.huggingface.cloud/" #Add a URL for a model of your choosing
-    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+class RAGPipeline():
+    def __init__(self) -> None:
+        self.API_TOKEN = os.environ["API_TOKEN"] 
+        self.API_URL = "https://z8dvl7fzhxxcybd8.eu-west-1.aws.endpoints.huggingface.cloud/"
 
-    payload = {
-        "inputs": prompt,
-        "parameters": { #Try and experiment with the parameters
-            "max_new_tokens": 1024,
-            "temperature": 0.6,
-            "top_p": 0.9,
-            "do_sample": False,
-            "return_full_text": False
+    def query(self, prompt):
+        headers = {"Authorization": f"Bearer {self.API_TOKEN}"}
+
+        payload = {
+            "inputs": prompt,
+            "parameters": { #Try and experiment with the parameters
+                "max_new_tokens": 1024,
+                "temperature": 0.6,
+                "top_p": 0.9,
+                "do_sample": False,
+                "return_full_text": False
+            }
         }
-    }
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()[0]['generated_text']
+        response = requests.post(self.API_URL, headers=headers, json=payload)
+        return response.json()[0]['generated_text']
 
-def as_retriever(collection, query):
-    results = collection.query(
-        query_texts=[query],
-        n_results=3
-    )
-    return results['documents']
+    def as_retriever(self, collection, query):
+        results = collection.query(
+            query_texts=[query],
+            n_results=3
+        )
+        return results['documents']
 
 def main():
+    rag_app = RAGPipeline()
     custom_embedder = CustomEmbedder()
     client = chromadb.PersistentClient(path="../testdb")
     collection = client.get_collection(name="test_collection", embedding_function=custom_embedder)
 
     question = "Do I have a pet?"
 
-    context = as_retriever(collection, question)
+    context = rag_app.as_retriever(collection, question)
     prompt = f"""Use the following context to answer the question at the end. Stop when you've answered the question. Do not generate any more than that.
 
     {context}
@@ -43,7 +47,7 @@ def main():
     Question: {question}
     """
 
-    print(query(prompt))
+    print(rag_app.query(prompt))
 
 
 if __name__ == "__main__":
--- embedder.py
+++ embedder.py
@@ -8,13 +8,15 @@ from chromadb.api.types import (
     Embeddings
 )
 
-class CustomEmbedder(EmbeddingFunction[Documents]):
+class CustomEmbedder(EmbeddingFunction):
+    def __init__(self) -> None:
+        self.API_TOKEN = os.environ["API_TOKEN"]
+        self.API_URL = "https://c9ejquzh6yum3xqf.us-east-1.aws.endpoints.huggingface.cloud/"
+
     def __call__(self, input: Documents) -> Embeddings:
         rest_client = requests.Session()
-        API_TOKEN = os.environ["API_TOKEN"] #Set a API_TOKEN environment variable before running
-        API_URL = "https://c9ejquzh6yum3xqf.us-east-1.aws.endpoints.huggingface.cloud/"
         response = rest_client.post(
-            API_URL, json={"inputs": input}, headers={"Authorization": f"Bearer {API_TOKEN}"}
+            self.API_URL, json={"inputs": input}, headers={"Authorization": f"Bearer {self.API_TOKEN}"}
         ).json()
         return response
 
@@ -24,18 +26,34 @@ def main():
         "I want to visit Italy.",
         "I need to download more RAM."
     ]
+    metadatas = [
+        {"doc_name": "testdoc"},
+        {"doc_name": "testdoc"},
+        {"doc_name": "testdoc"}
+    ]
+    ids = [
+        "1",
+        "2",
+        "3"
+    ]
     chroma_client = chromadb.PersistentClient(path="../testdb")
 
     custom_embedder = CustomEmbedder()
     chroma_client.delete_collection("test_collection")
     collection = chroma_client.create_collection(name="test_collection", embedding_function=custom_embedder)
     
-    for i, dataset_id in enumerate(dataset):
-        collection.add(
-            documents=[dataset[i]],
-            metadatas=[{"doc_name": "testdoc"}],
-            ids=[str(i)]
-        )
+    collection.add(
+        documents=dataset,
+        metadatas=metadatas,
+        ids=ids
+    )
+
+    # for i, dataset_id in enumerate(dataset):
+    #     collection.add(
+    #         documents=[dataset[i]],
+    #         metadatas=[{"doc_name": "testdoc"}],
+    #         ids=[str(i)]
+    #     )
 
     results = collection.query(
         query_texts=["travel"],
